a5157ffa5fdfd2a6d445d59829efd5168e53065c
Yu-Geun-Chan <geunchan9055@gmail.com>
2024-10-15 16:44:33 +0900

    Configuration 수정, 배치 관련 DB, incrementer 설정

--- src/main/java/com/koreait/exam/springbatch_10/job/HelloWorld/HelloWorldJobConfig.java
+++ src/main/java/com/koreait/exam/springbatch_10/job/HelloWorld/HelloWorldJobConfig.java
@@ -6,6 +6,7 @@ import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.context.annotation.Bean;
@@ -13,13 +14,15 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @RequiredArgsConstructor
-public class HelloWorldConfig {
+public class HelloWorldJobConfig {
     private final JobBuilderFactory jobBuilderFactory; // Job을 빌드해줄 빌더를 만들어준다.
     private final StepBuilderFactory stepBuilderFactory; // Step을 빌드해줄 빌더를 만들어준다.
 
     @Bean
     public Job helloWorldJob() {
-        return jobBuilderFactory.get("helloWorldJob").start(helloWorldStep1()).build();
+        return jobBuilderFactory.get("helloWorldJob")
+                .incrementer(new RunIdIncrementer()) // 강제로 메번 다른 ID를 실행할 때 마다 파라미터로 부여
+                .start(helloWorldStep1()).build();
     }
 
     @Bean
@@ -30,11 +33,10 @@ public class HelloWorldConfig {
     @Bean
     public Tasklet helloWorldTasklet() {
         return (stepContribution, chunkContext) -> {
-            System.out.println("helloWorld!!!");
+            System.out.println("헬로월드!!!");
             return RepeatStatus.FINISHED;
         };
     }
-
     // Job : 여러가지의 Step들로 구성
 }
 
