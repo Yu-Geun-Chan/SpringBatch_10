46028cffacdba2f2b624380bbbfdeb05712b2d7b
Yu-Geun-Chan <geunchan9055@gmail.com>
2024-10-21 13:05:14 +0900

    productBackupJob 구현, backup 데이터 생성

---
+++ src/main/java/com/koreait/exam/springbatch_10/app/product/entity/ProductBackup.java
@@ -0,0 +1,38 @@
+package com.koreait.exam.springbatch_10.app.product.entity;
+
+import com.koreait.exam.springbatch_10.app.entity.BaseEntity;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import lombok.experimental.SuperBuilder;
+
+import javax.persistence.Entity;
+import javax.persistence.OneToOne;
+
+import static javax.persistence.FetchType.LAZY;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@SuperBuilder
+@ToString(callSuper = true)
+public class ProductBackup extends BaseEntity {
+    private String name;
+    private int salePrice;
+    private int price;
+    private int wholesalePrice;
+    private String makerShopName;
+    private boolean isSoldOut; // 품절 여부
+    @OneToOne(fetch = LAZY)
+    private Product product;
+    public ProductBackup(Product product) {
+        this.product = product;
+        salePrice = product.getSalePrice();
+        price = product.getPrice();
+        wholesalePrice = product.getWholesalePrice();
+        makerShopName = product.getMakerShopName();
+        name = product.getName();
+        isSoldOut = product.isSoldOut();
+    }
+}
